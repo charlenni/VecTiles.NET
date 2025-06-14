0c86e615a846d29674558dda917ad9a2e113e433
Charlenni <web@weltz-online.de>
2025-06-14 12:57:36 +0200

    Add benchmarking project for Mapbox tile converter
    Updated the solution to include a new "Benchmarks" project and the "VecTiles.Converters.Mapbox.Benchmark" project. Created `MapboxConverterBenchmarks.cs` for performance testing using BenchmarkDotNet. Modified `Program.cs` to execute benchmarks and configured `VecTiles.Converters.Mapbox.Benchmark.csproj` with necessary references and file links.

---
+++ Benchmarks/VecTiles.Converters.Mapbox.Benchmark/MapboxConverterBenchmarks.cs
@@ -0,0 +1,62 @@
+﻿using BenchmarkDotNet.Attributes;
+using VecTiles.Common.Interfaces;
+using VecTiles.Common.Primitives;
+using VecTiles.DataSources.MbTiles;
+
+namespace VecTiles.Converters.Mapbox.Benchmarks;
+
+[MemoryDiagnoser]
+public class MapboxConverterBenchmarks
+{
+    readonly string _path = "files\\zurich.mbtiles";
+
+    ITileConverter? _tileConverter;
+    List<Tile> _tiles = new List<Tile> { new Tile(1072, 717, 11), new Tile(8580, 5738, 14), new Tile(8581, 5738, 14), new Tile(8580, 5739, 14) };
+    List<byte[]?> _data = new List<byte[]?>();
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var dataSource = new MbTilesTileDataSource(_path);
+
+        _tileConverter = new MapboxTileConverter();
+
+        foreach (var tile in _tiles)
+            _data.Add(dataSource.GetTileAsync(tile).ConfigureAwait(false).GetAwaiter().GetResult());
+    }
+
+    [Benchmark]
+    [Arguments(0)]
+    [Arguments(1)]
+    [Arguments(2)]
+    [Arguments(3)]
+    public void ReadVectorTile(int i)
+    {
+        var data = _data[i];
+
+        if (data != null)
+        {
+            _tileConverter?.Convert(_tiles[i], _tiles[i], data)
+                .ConfigureAwait(false)
+                .GetAwaiter()
+                .GetResult();
+        }
+    }
+
+    [Benchmark]
+    public void ReadVectorTiles()
+    {
+        for (var i = 0; i < _tiles.Count(); i++)
+        {
+            var data = _data[i];
+
+            if (data != null)
+            {
+                _tileConverter?.Convert(_tiles[i], _tiles[i], data)
+                    .ConfigureAwait(false)
+                    .GetAwaiter()
+                    .GetResult();
+            }
+        }
+    }
+}
---
+++ Benchmarks/VecTiles.Converters.Mapbox.Benchmark/Program.cs
@@ -0,0 +1,4 @@
+﻿using BenchmarkDotNet.Running;
+using VecTiles.Converters.Mapbox.Benchmarks;
+
+BenchmarkRunner.Run<MapboxConverterBenchmarks>();

2aaf63ee828482c46fbc6234f584779b29866c7a
Charlenni <web@weltz-online.de>
2025-06-14 13:27:49 +0200

    Add Scheme enum for tile addressing in VecTiles
    Introduced a new namespace `VecTiles.Common.Enums` and added a public enum `Scheme` to represent tile addressing schemes, including `Tms` and `Xyz`.

---
+++ Source/VecTiles.Common/Enums/Scheme.cs
@@ -0,0 +1,16 @@
+﻿namespace VecTiles.Common.Enums;
+
+/// <summary>
+/// Represents the tile addressing scheme.
+/// </summary>
+public enum Scheme
+{
+    /// <summary>
+    /// TMS tile scheme (Tile Map Service, origin at bottom-left).
+    /// </summary>
+    Tms,
+    /// <summary>
+    /// XYZ tile scheme (OpenStreetMap, origin at top-left).
+    /// </summary>
+    Xyz
+}

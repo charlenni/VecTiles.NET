7d736b84b0f2395fe3b6f0d4e0e0f6f43317aec8
Charlenni <web@weltz-online.de>
2026-02-02 12:41:25 +0100

    Add some tests

---
+++ Tests/VecTiles.Converters.Tests/OMTFontConverter.Tests.cs
@@ -0,0 +1,78 @@
+﻿using VecTiles.Converters.SdfFont;
+
+namespace VecTiles.Converters.Tests;
+
+public class OMTFontConverterTests
+{
+    readonly string _path = "files\\fonts\\roboto condensed italic\\";
+
+    [Fact]
+    public async Task FontConverter_0_255_Test()
+    {
+        var data = File.ReadAllBytes(_path + "0-255.pbf");
+
+        var fontConverter = new SdfFontConverter();
+        var fontGlyphs = await fontConverter.Convert(data);
+
+        Assert.True(fontGlyphs != null);
+        Assert.True(fontGlyphs.Stacks != null);
+        Assert.True(fontGlyphs.Stacks.Count == 1);
+        Assert.True(fontGlyphs.Stacks[0] != null);
+        Assert.True(fontGlyphs.Stacks[0].Name == "Roboto Condensed Italic");
+        Assert.True(fontGlyphs.Stacks[0].Range == "0-255");
+        Assert.True(fontGlyphs.Stacks[0].Glyph.Count == 194);
+
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Id == 49);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Width == 6);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Height == 17);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Left == 6);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Top == 15);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Advance == 11);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Bitmap.Length == 276);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Bitmap[0] == 21);
+
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Id == 93);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Width == 7);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Height == 24);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Left == 1);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Top == 9);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Advance == 5);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Bitmap.Length == 390);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Bitmap[0] == 0);
+    }
+
+    [Fact]
+    public async Task FontConverter_1024_1279_Test()
+    {
+        var data = File.ReadAllBytes(_path + "1024-1279.pbf");
+
+        var fontConverter = new SdfFontConverter();
+        var fontGlyphs = await fontConverter.Convert(data);
+
+        Assert.True(fontGlyphs != null);
+        Assert.True(fontGlyphs.Stacks != null);
+        Assert.True(fontGlyphs.Stacks.Count == 1);
+        Assert.True(fontGlyphs.Stacks[0] != null);
+        Assert.True(fontGlyphs.Stacks[0].Name == "Roboto Condensed Italic");
+        Assert.True(fontGlyphs.Stacks[0].Range == "1024-1279");
+        Assert.True(fontGlyphs.Stacks[0].Glyph.Count == 255);
+
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Id == 1044);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Width == 15);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Height == 21);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Left == 1);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Top == 15);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Advance == 15);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Bitmap.Length == 567);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[20].Bitmap[0] == 0);
+
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Id == 1088);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Width == 11);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Height == 18);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Left == 1);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Top == 23);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Advance == 11);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Bitmap.Length == 408);
+        Assert.True(fontGlyphs.Stacks[0].Glyph[64].Bitmap[0] == 0);
+    }
+}
---
+++ Tests/VecTiles.Renderers.Test/SkiaRendererTests.cs
@@ -0,0 +1,88 @@
+﻿using SkiaSharp;
+using VecTiles.Common.Primitives;
+using VecTiles.Converters.OpenMapTiles;
+using VecTiles.DataSources.MbTiles;
+using VecTiles.Renderers.Common;
+using VecTiles.Renderers.Skia;
+using VecTiles.Styles.OpenMapTiles;
+using VecTiles.TileDataSources;
+
+namespace VecTiles.Renderers.Test;
+
+public class SkiaRendererTests
+{
+    private const string Folder = "files";
+    private readonly string _dataFile = Path.Combine(Folder, "zurich.mbtiles");
+    private readonly string _styleFile = Path.Combine(Folder, "osm-liberty.json");
+    private readonly Renderer _renderer;
+
+    public SkiaRendererTests()
+    {
+        var dataSource = new MbTilesTileDataSource(_dataFile, determineZoomLevelsFromTilesTable: true, determineTileRangeFromTilesTable: true);
+        var converter = new OMTTileConverter();
+        var vectorDataSource = new VectorTileDataSource(dataSource, converter);
+
+        var omtStyleStream = File.Open(_styleFile, FileMode.Open, FileAccess.Read);
+        var omtStyleFile = OMTStyleFileLoader.Load(omtStyleStream).Result;
+
+        var spriteDictionary = omtStyleFile.Sprites;
+        var paintFactory = new OMTPaintFactory(spriteDictionary);
+        var symbolFactory = new OMTSymbolFactory(spriteDictionary);
+        var renderFactory = new RenderFactory(omtStyleFile.Layers, paintFactory, symbolFactory);
+
+        _renderer = new Renderer([vectorDataSource],  omtStyleFile.Layers, renderFactory);
+    }
+
+
+    [Theory]
+    [InlineData(0, 0, 0)]
+    [InlineData(1, 0, 1)]
+    [InlineData(2, 1, 2)]
+    [InlineData(4, 2, 3)]
+    [InlineData(8, 5, 4)]
+    [InlineData(16, 11, 5)]
+    [InlineData(33, 22, 6)]
+    [InlineData(67, 44, 7)]
+    [InlineData(134, 89, 8)]
+    [InlineData(268, 179, 9)]
+    [InlineData(536, 358, 10)]
+    [InlineData(1072, 717, 11)]
+    [InlineData(2145, 1434, 12)]
+    [InlineData(4290, 2869, 13)]
+    [InlineData(8580, 5738, 14)]
+    [InlineData(17161, 11476, 15)]
+    [InlineData(34323, 22952, 16)]
+    [InlineData(68646, 45904, 17)]
+    [InlineData(137293, 91809, 18)]
+    [InlineData(274587, 183619, 19)]
+    [InlineData(549174, 367239, 20)]
+    public async Task CreateTileTest(int x, int y, int zoom)
+    {
+        var tile = new Tile(x, y, zoom);
+
+        try
+        {
+            var renderedTile = (RenderedTile)(await _renderer.Render(tile));
+            var renderContext = new EvaluationContext(tile.Zoom);
+
+            var imageInfo = new SKImageInfo {Width = 512, Height = 512, ColorType = SKColorType.Rgba8888, AlphaType = SKAlphaType.Premul};
+
+            using var bitmap = new SKBitmap(imageInfo);
+            using (var canvas = new SKCanvas(bitmap))
+            {
+                renderedTile?.Draw(canvas, renderContext);
+            }
+
+            using (var image = SKImage.FromBitmap(bitmap))
+            using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+            await using (var stream = File.OpenWrite(Path.Combine(Folder, $"Tile-{x}x-{y}y-{zoom}z.png")))
+            {
+                data.SaveTo(stream);
+            }
+        }
+        catch (Exception e)
+        {
+            System.Diagnostics.Debug.WriteLine($"Tile {tile} couldn't be rendered");
+        }
+    }
+}

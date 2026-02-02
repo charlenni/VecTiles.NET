ddedf10861462e2872a8881b8519c1128c5eab76
Charlenni <web@weltz-online.de>
2026-02-02 12:38:08 +0100

    Add some more files

---
+++ Source/VecTiles.Common/Extensions/AnchorExtensions.cs
@@ -0,0 +1,24 @@
+﻿using NetTopologySuite.Geometries;
+using VecTiles.Common.Enums;
+
+namespace VecTiles.Common.Extensions;
+
+public static class AnchorExtensions
+{
+    public static Point ToPoint(this Anchor anchor)
+    {
+        return anchor switch
+        {
+            Anchor.Center => new Point(-0.5f, -0.5f),
+            Anchor.Left => new Point(0, -0.5f),
+            Anchor.Right => new Point(-1.0f, -0.5f),
+            Anchor.Top => new Point(-0.5f, 0),
+            Anchor.Bottom => new Point(-0.5f, -1.0f),
+            Anchor.TopLeft => new Point(0, 0),
+            Anchor.TopRight => new Point(-1.0f, 0),
+            Anchor.BottomLeft => new Point(0, -1.0f),
+            Anchor.BottomRight => new Point(-1.0f, -1.0f),
+            _ => throw new NotImplementedException($"Unknown Anchor")
+        };
+    }
+}
---
+++ Source/VecTiles.Common/Extensions/FloatExtensions.cs
@@ -0,0 +1,19 @@
+﻿using NetTopologySuite.Geometries;
+
+namespace VecTiles.Common.Extensions;
+
+public static class FloatExtensions
+{
+    public static Point ToPoint(this float[] offset, float scale = 1.0f)
+    {
+        if (offset.Length == 2)
+        {
+            return new Point(offset[0] * scale, offset[1] * scale);
+        }
+        else
+        {
+            return new Point(0, 0);
+        }
+    }
+
+}

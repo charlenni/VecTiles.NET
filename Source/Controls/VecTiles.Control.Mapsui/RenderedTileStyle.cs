1b4233f5ecd5084d5cdc6e28890f27d989d02f00
Charlenni <web@weltz-online.de>
2026-02-26 11:23:44 +0100

    Update to latest Mapsui beta because of Avalonia

--- Source/Controls/VecTiles.Control.Mapsui/RenderedTileStyle.cs
+++ Source/Controls/VecTiles.Control.Mapsui/RenderedTileStyle.cs
@@ -4,6 +4,8 @@ namespace VecTiles.Controls.Mapsui;
 
 public class RenderedTileStyle() : IStyle
 {
+    private static long _currentStyleId; // last used style id
+
     public double MinVisible { get; set; } = 0;
 
     public double MaxVisible { get; set; } = double.MaxValue;
@@ -11,4 +13,19 @@ public class RenderedTileStyle() : IStyle
     public bool Enabled { get; set; } = true;
 
     public float Opacity { get; set; } = 1.0f;
+
+    /// <inheritdoc />
+    public long GenerationId { get; private set; }
+
+    /// <inheritdoc />
+    public virtual void Modified()
+    {
+        // Modified needs a new id to invalidate cached drawables.
+        GenerationId = NextId();
+    }
+
+    private static long NextId()
+    {
+        return Interlocked.Increment(ref _currentStyleId);
+    }
 }
